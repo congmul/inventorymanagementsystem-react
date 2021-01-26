78f5620de08e86562af26374b063f1eebf09f0c5
Jehyun <congmul@hotmail.com>
2021-01-26 08:07:10 -0800

    Use react-router-dom

---
+++ src/components/Cymbals.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Cymbals() {
+    return (
+        <div>
+            <h1>Router Test!!</h1>
+        </div>
+    )
+}
--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,21 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
 import NavLeft from './NavLeft'
 import Footer from './Footer'
 import Dashboard from './Dashboard'
+import Cymbals from './Cymbals'
 
 export default function Frame() {
     return (
-        <>
+        <Router>
         <div className="row" style={{backgroundColor: "#e6e6e6", marginTop:"65px", paddingTop: "60px", height:"100vh"}}>
             <div className="col-2 pr-0" style={{minWidth:"200px"}}>
                 <NavLeft />
             </div>
             <div className="col-10 pl-0">
-                <Dashboard />
-                
+                <Route exact path="/" component={Dashboard} />
+                <Route exact path="/cymbals" component={Cymbals} />
+
             </div>
         </div>
         <Footer />
-        </>
+        </Router>
     )
 }
