e9b63f06ba28fc54e129c96b397ac7fbe0028b83
Jehyun <congmul@hotmail.com>
2021-01-25 23:18:50 -0800

    Complete Dashboard

--- src/components/Frame.js
+++ src/components/Frame.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import NavLeft from './NavLeft'
 import Footer from './Footer'
+import Dashboard from './Dashboard'
 
 export default function Frame() {
     return (
         <>
         <div className="row" style={{backgroundColor: "#e6e6e6", marginTop:"65px", paddingTop: "60px", height:"100vh"}}>
-            <div className="col-3">
+            <div className="col-2 pr-0" style={{minWidth:"200px"}}>
                 <NavLeft />
             </div>
-            <div className="col-9">
-                <h1>TEST01</h1>
-                <h1>TEST01</h1>
-                <h1>TEST01</h1>
-                <h1>TEST01</h1>
+            <div className="col-10 pl-0">
+                <Dashboard />
+                
             </div>
         </div>
         <Footer />
