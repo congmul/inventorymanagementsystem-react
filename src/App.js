294691e306202a0aaf3281b3d8d87abfa94db488
Jehyun <congmul@hotmail.com>
2021-01-25 20:58:26 -0800

    Add initial NPM packages and index.js / app.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+           <h1>Initial TEXT <i class="bi bi-alarm"></i></h1>
+        </div>
+    )
+}
+
+export default App
