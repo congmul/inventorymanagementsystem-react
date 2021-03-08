8cbdd26b4702ee347b11415b365ef88449ebb2b2
Jehyun <congmul@hotmail.com>
2021-03-08 10:29:01 -0800

    Modified Configuration for Server

--- server.js
+++ server.js
@@ -1,50 +1,27 @@
-const express = require('express');
-const logger = require("morgan");
-const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
 
-const PORT = process.env.PORT || 4000;
-
-const db = require("./models");
-
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
-
-app.use(logger("dev"));
-app.use(cors());
-// app.use(bodyParser.json());
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+// Add routes, both API and view
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/wavemgDB", 
 { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useCreateIndex: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
-
-mongoose.connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
-})
-
-app.get("/", (req, res) => {
-    console.log("Get Server")
-    db.Orders.find({})
-    .then(res => {
-      console.log("TEXT")
-      res.json(res);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
-});
-
-// routes
-app.use(require("./routes/orders.js"));
-app.use(require("./routes/products.js"));
 
 app.listen(PORT, () => {
     console.log("Server is running on Port: " + PORT);
